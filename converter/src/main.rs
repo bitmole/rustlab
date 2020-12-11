1f6a172d06e95ce41db53584943e80fef1f1e03e
Robert Hanacek <robert@idea-loop.com>
2020-12-10 17:26:03 -0700

    Use structs to clarify program.

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,64 +1,78 @@
-//use std::io;
 use std::env;
-use std::collections::HashMap;
+use std::process;
 
 struct Unit {
-    label: String,
+    code: String,
     base_value: f32,
 }
 
-struct Config {
-    units: f32,
-    unit_code: String,
+impl Unit {
+    fn convert(&self, distance: &Distance) -> Distance {
+        Distance { 
+            n: (distance.n * distance.unit.base_value) / self.base_value, 
+            unit: self 
+        }
+    }
+}
+
+struct Distance<'a> {
+    n: f32,
+    unit: &'a Unit,
+}
+
+impl Distance<'_> {
+    fn new(mut args: env::Args, supported_units: &[Unit]) -> Result<Distance, &'static str> {
+        args.next(); // program name
+
+        let n: f32 = match args.next() {
+            Some(arg) => arg.parse().unwrap(),
+            None => return Err("Didn't get number of units.")
+        };
+
+        let unit_code = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get unit code.")
+        };
+
+        let unit = match supported_units
+                    .iter()
+                    .filter(|u| u.code == unit_code)
+                    .next() {
+            Some(u) => u,
+            None => return Err("Unsupported unit. Aborting.")
+        };
+
+        Ok(Distance { n: n, unit: unit })
+            
+    }
 }
 
 fn main() {
 
     // init
     let units = vec![
-        Unit { label: String::from("mi"), base_value: 160_934.0 },
-        Unit { label: String::from("km"), base_value: 100_000.0 },
-        Unit { label: String::from("m"), base_value: 100.0 },
-        Unit { label: String::from("ft"), base_value: 30.48 },
-        Unit { label: String::from("in"), base_value: 2.54 },
-        Unit { label: String::from("cm"), base_value: 1.0 },
-        Unit { label: String::from("mm"), base_value: 0.1 },
+        Unit { code: String::from("mi"), base_value: 160_934.0 },
+        Unit { code: String::from("km"), base_value: 100_000.0 },
+        Unit { code: String::from("m"), base_value: 100.0 },
+        Unit { code: String::from("ft"), base_value: 30.48 },
+        Unit { code: String::from("in"), base_value: 2.54 },
+        Unit { code: String::from("cm"), base_value: 1.0 },
+        Unit { code: String::from("mm"), base_value: 0.1 },
     ];
 
-    let unit_map: HashMap<_, _> = units.iter()
-            .map(|item| (item.label.as_str(), item))
-            .collect();
-
-    // user input
-    let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-
-    let source_unit = match unit_map.get(&config.unit_code[..]) {
-        Some(&u) => u,
-        None => {
-            println!("Unsupported unit '{}'. Aborting.", config.unit_code);       
-            return;
-        }
-    };
+    let source_distance = Distance::new(env::args(), &units).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
    // results
-    println!("\n{}{}=", config.units, config.unit_code);
+    println!("\n{}{}=", source_distance.n, source_distance.unit.code);
 
     for target_unit in units.iter() {
-        if target_unit.label == source_unit.label { continue; }
-        println!("\t{}{}", 
-                 convert(&config, &source_unit, &target_unit),
-                 target_unit.label);
+        if target_unit.code == source_distance.unit.code { continue; }
+        
+        let target_distance = target_unit.convert(&source_distance);
+
+        println!("\t{}{}", target_distance.n, target_distance.unit.code);
     }
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let units: f32 = (&args[1]).parse().expect("Please enter a number!");
-    let unit_code = args[2].clone();
-
-    Config { units, unit_code }
-}
-
-fn convert(config: &Config, source: &Unit, target: &Unit) -> f32 {
-    (config.units * source.base_value) / target.base_value
 }
